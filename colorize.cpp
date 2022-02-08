99dcc15c84f120be1c9999662f31d5085abaf586
Simon946 <56819218+Simon946@users.noreply.github.com>
2022-02-08 21:46:40 +0100

    Added support for '{' and '['

--- colorize.cpp
+++ colorize.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stack>
 
-const int COLOR_CONTRAST = 1;
-
-void coloredPrint(std::string input, int color){
+void coloredPrint(std::string input, int color, int contrast){
     const std::string colorEnd = "\033[0m";
 
     for(int i = 0; i < input.length(); i++){
         std::string colorBegin = "\033[38;5;";
         
-        if(input.at(i) == '('){
-            color+=COLOR_CONTRAST;
+        if(input.at(i) == '(' || input.at(i) == '[' || input.at(i) == '{'){
+            color+=contrast;
             colorBegin += std::to_string(color);
             colorBegin += "m";
             input.insert((i), colorBegin);
             i += colorBegin.length();
         }
-        else if(input.at(i) == ')'){
+        else if(input.at(i) == ')' || input.at(i) == ']' || input.at(i) == '}'){
 
-            if(input.find(")", i + 1) == std::string::npos){
+            if(input.find(")", i + 1) == std::string::npos && input.find("]", i + 1) == std::string::npos && input.find("}", i + 1) == std::string::npos){
                 input.insert(i + 1, colorEnd);
                 i = input.length();
             }
             else{
                 input.insert(i + 1, colorEnd);
                 std::string newColorBegin = "\033[38;5;";
-                color -= COLOR_CONTRAST;
+                color -= contrast;
                 newColorBegin += std::to_string(color);
                 newColorBegin += "m";
                 input.insert(i + 1 + colorEnd.length(), newColorBegin);
@@ -38,8 +37,38 @@ void coloredPrint(std::string input, int color){
     std::cout << input << std::endl;
 }
 
+bool validate(std::string input){
+    std::stack<char> brackets;
+
+    for (int i = 0; i < input.length(); i++){
+        char a = input.at(i);
+        switch (a)
+        {
+        case '(':
+            brackets.push(')');
+            break;
+        case '[':
+            brackets.push(']');
+            break;
+        case '{':
+            brackets.push('}');
+            break;
+        case ')':
+        case ']':
+        case '}':
+                {
+                if(brackets.empty() || brackets.top() != a) return false;
+                else brackets.pop();
+            }
+        default:
+            break;
+        }
+    }
+    return brackets.empty();
+}
+
 int main(int argc, char* argv[]){
-    if (argc != 2){
+    if (argc < 2){
         std::cerr << "Example usage: ./colorize \"q\\/(p -> t)\"" << std::endl;
         return 1;
     }
@@ -54,6 +83,11 @@ int main(int argc, char* argv[]){
             input.push_back(letter);
         }
     }
-    coloredPrint(input, 3);
+    if(!validate(input)) std::cerr << "Warning! unbalanced brackets" << std::endl;
+    int color = 0;
+    int contrast = 1;
+    if (argc >= 3)color = std::stoi(argv[2]);
+    if (argc >= 4)contrast = std::stoi(argv[3]);
+    coloredPrint(input, color, contrast);
     return 0;
 }
